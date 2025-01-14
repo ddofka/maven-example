6e791a33be2c82c13c4c0fa9bc9a04aed98bf93c
ddofka <ddorofejevas@yahoo.com>
2025-01-14 21:58:46 +0200

    Ints methods examples

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import com.google.common.collect.Range;
+import com.google.common.primitives.Ints;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,6 +13,14 @@ public class Main {
         System.out.println(semiUzdaras.contains(15));
         System.out.println(uzdaras.intersection(semiUzdaras));
         System.out.println(uzdaras.span(semiUzdaras));
+
+        int[] skaiciai = {1,2,3,45,15123,123,125,555,-10};
+
+        System.out.println(Ints.min(skaiciai));
+        System.out.println(Ints.max(skaiciai));
+        System.out.println(Ints.contains(skaiciai,3));
+        System.out.println(Ints.contains(skaiciai,5));
+        System.out.println(Ints.asList(skaiciai).toArray());
 
     }
 }
